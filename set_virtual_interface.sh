78e78fbf26ebda622908635011ac55d0f3c8715d
RuchitJagodara <ruchit.jagodara@iitgn.ac.in>
2025-02-01 19:41:47 +0530

    a small change

--- set_virtual_interface.sh
+++ set_virtual_interface.sh
@@ -3,8 +3,3 @@ sudo nmcli dev set veth0 managed no
 sudo nmcli dev set veth1 managed no
 sudo ip link set veth0 up
 sudo ip link set veth1 up
-# sudo ip netns add ns1
-# sudo ip link set veth0 netns ns1
-# sudo ip netns exec ns1 ip link set veth0 up
-# sudo ip link set veth1 netns ns1
-# sudo ip netns exec ns1 ip link set veth1 up
